d8ea49344263213bd359f3a23377e9268ff7ff58
Angoty <angotyrabarijaona@gmail.com>
2023-05-21 14:44:11 +0200

    third commit

---
+++ src/java/exe.bat
@@ -0,0 +1,6 @@
+javac -d . classeAnnoted/AppRoute.java
+javac -d . Annotations.java
+javac -d . Mapping.java
+javac -d . MappingUrls.java
+javac -d . FrontServlet.java
+cd E:\L2\S4\Mr Naina\Update\Framework\sprint4
